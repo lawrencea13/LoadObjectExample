ae558631b0c3b2c1b27967385f834d6300509efa
lawrencea13 <105841326+lawrencea13@users.noreply.github.com>
2023-11-12 10:52:52 -0500

    Uploaded source

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LoadObjectExample.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LoadObjectExample
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ExampleBaseClass : BaseUnityPlugin
+    {
+        private const string modGUID = "Posiedon.ExampleObjectLoader";
+        private const string modName = "Example Object Loader";
+        private const string modVersion = "1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        public static ManualLogSource mls;
+
+        private static ObjectToLoad myLoadedObject;
+
+        void Awake()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("Example Object Loader");
+            mls.LogInfo("Loaded Example. Patching.");
+            harmony.PatchAll(typeof(ExampleBaseClass));
+            mls = Logger;
+
+            // probably come up with better name
+            var gameObject = new UnityEngine.GameObject("MyGameObject");
+            // these 2 lines are important
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+            gameObject.AddComponent<ObjectToLoad>();
+            myLoadedObject = (ObjectToLoad)gameObject.GetComponent("ObjectToLoad");
+
+            myLoadedObject.DoSomething();
+        }
+
+    }
+}
---
+++ ObjectToLoad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LoadObjectExample.Component
+{
+    internal class ObjectToLoad : MonoBehaviour
+    {
+        void Awake()
+        {
+            ExampleBaseClass.mls.LogInfo("Object example loaded.");
+        }
+
+        void OnDestroy()
+        {
+            ExampleBaseClass.mls.LogInfo("Object example destroyed.");
+        }
+
+        public void DoSomething()
+        {
+            ExampleBaseClass.mls.LogInfo("The example object has something to say :)");
+        }
+
+        public void Update()
+        {
+            ExampleBaseClass.mls.LogInfo("Update function called automatically by the game.");
+        }
+    }
+}
